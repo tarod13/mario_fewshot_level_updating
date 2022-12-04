7d298e173048d8835ae51b16073a6f8acc7c8871
Diego Gomez <df.gomez11@uniandes.edu.co>
2022-12-04 02:20:55 -0600

    Added SMB levels and dataset methods
    - Added original SMB levels with two different types of tokens.
    - Added some preliminar methods to generate the three necessary datasets.

--- src/utils/data.py
+++ src/utils/data.py
@@ -1,6 +1,9 @@
+import os
 import numpy as np
 import torch as th
 from torch.utils.data import TensorDataset, DataLoader
+from torch.nn.functional import one_hot
+from itertools import combinations
 
 
 def load_data(
@@ -58,9 +61,170 @@ def generate_dataset(
     return mario_train, mario_val, token_frequencies
 
 
+def level_to_list(level_txt):
+    '''Returns a list by splitting the level text by \n.'''
+    as_list = level_txt.split("\n")
+    return [list(row) for row in as_list if row != ""]
+
+def level_to_array(level_list):
+    '''Returns an np array'''
+    result = np.array(level_list, dtype=(int))
+    return (result)
+
+def level_tokenizer(original_rows, number_rows, dictionary):
+    for level_row in original_rows:
+        for i in range(len(level_row)):
+            token = level_row[i]
+            number = dictionary[token]
+            level_row[i] = number
+        number_rows.append(level_to_array(level_row))
+        
+def narray_to_tensor(
+    level_narray, tensor_list, 
+    size: int = 20, 
+    stride: int = 5, 
+    num_classes: int = 13
+):
+    level_tensor = th.from_numpy(level_narray)
+    level_tensor = one_hot(level_tensor.to(th.int64), num_classes=num_classes)
+    level_tensor = level_tensor.unfold(1, size, stride)
+    tensor_list.append(level_tensor)
+    
+def unify_save_tensor(tensor_list, file):
+    unified_tensor_list = th.cat(tensor_list, dim=1)\
+        .transpose(0,1).transpose(1,2)
+    print(unified_tensor_list.shape)
+    np.savez(file, unified_tensor_list)
+
+def find_possible_transformations(transformation_basis: list, r: int):
+    possible_transformations = list(combinations(transformation_basis, r=r))
+    possible_transformations = list(set([
+        tuple(set(transform)) for transform in possible_transformations
+    ]))
+    possible_transformations.sort(key=len)
+    return possible_transformations
+
+def update_keys_of_dict(old_dict, key_dict):
+    new_dict = old_dict.copy()
+    for k, v in key_dict.items():
+        if k in new_dict:
+            new_dict[k] = v
+    return new_dict
+
+def generate_token_dicts(token_dicts, transformation_dict):
+    transformation_pairs = []
+    for transform_key, transform_value in transformation_dict.items():
+        token_dicts_to_append = dict()
+        for d_key, d_value in token_dicts.items():
+            if 'original' in d_key:
+                new_d_key = frozenset((transform_key,))
+            else:
+                new_d_key = frozenset.union(d_key, frozenset((transform_key,)))
+            transformation_pairs.append((new_d_key, d_key))
+            new_d_value = update_keys_of_dict(d_value, transform_value)
+            token_dicts_to_append[new_d_key] = new_d_value
+        token_dicts = token_dicts | token_dicts_to_append   # Python 3.9 and onwards
+    return token_dicts, transformation_pairs
+
+def complete_transformation_pairs(transformation_pairs):
+    transformation_path_dict = {}
+    for origin, end in transformation_pairs:
+        transformation_path_dict[origin] = [end]
+        if len(origin) > 1:
+            for element in origin:
+                potential_end = frozenset(set(origin).difference(set((element,))))
+                if potential_end != end:
+                    transformation_path_dict[origin].append(potential_end)
+    return transformation_path_dict
+
+def simplify_token_dict_keys(token_dicts, transformation_path_dict):
+    remaining_keys = list(token_dicts.keys())
+    renamed_token_dicts = {}
+    renamed_pairs = []
+    for origin, ends in transformation_path_dict.items():
+        origin_str = str.join('-', list(origin))
+        if origin in remaining_keys:
+            remaining_keys.remove(origin)            
+            renamed_token_dicts[origin_str] = token_dicts[origin]
+        for end in ends:
+            end_str = str.join('-', list(end))
+            renamed_pairs.append((origin_str, end_str))
+            if end in remaining_keys:
+                remaining_keys.remove(end)                
+                renamed_token_dicts[end_str] = token_dicts[end]
+    return renamed_token_dicts, renamed_pairs
+
+
+def generate_dataset_from_original_levels(
+    path: str = './data/basic_13_tokens'
+):  
+    tensor_list = []
+    replaced_tensor_list = []
+    
+    # Basic representation
+    token_dict = {
+        "X": "0","S": "1","-": "2",
+        "?": "3","Q": "4","E": "5",
+        "<": "6",">": "7","[": "8",
+        "]": "9","o": "10","b": "11",
+        "B": "12"
+    }
+    
+    token_dicts = {
+        frozenset(('original',)): token_dict,
+    }
+
+    transformation_dict = {
+        'no_q_mark': {'Q':'S','?':'S'},
+        'no_cannon': {'b':'E','B':'E'},
+        'no_coins': {'o':'-'},
+    }
+
+    token_dicts, transformation_pairs = generate_token_dicts(
+        token_dicts, transformation_dict
+    )    
+    transformation_path_dict = complete_transformation_pairs(
+        transformation_pairs
+    )
+
+    token_dicts, transformation_pairs = simplify_token_dict_keys(
+        token_dicts, transformation_path_dict
+    )
+
+    print(len(transformation_pairs))
+    
+    # tokens_to_replace = {'?', 'Q'}
+    # replace_with = 'S'
+
+    # for i in tokens_to_replace:
+    #     token_dict_replaced[i] = token_dict[replace_with]
+
+    # for levelFile in os.listdir(path):
+    #     print ("Processing: " + levelFile)   #print level being loaded
+    #     with open(path+ '/' + levelFile) as fp:
+    #         level_txt = fp.read()
+    #         list_rows = level_to_list(level_txt)
+    #         list_rows_replaced = level_to_list(level_txt)
+            
+    #     list_rows_tf_replaced = []
+    #     level_tokenizer(list_rows_replaced, list_rows_tf_replaced, token_dict_replaced) 
+    #     level_narray_replaced = level_to_array(list_rows_tf_replaced)
+    #     narray_to_tensor(level_narray_replaced, replaced_tensor_list)
+        
+    #     list_rows_tf = []
+    #     level_tokenizer(list_rows, list_rows_tf, token_dict)
+    #     level_narray = level_to_array(list_rows_tf)
+    #     narray_to_tensor(level_narray, tensor_list)
+
+    # unify_save_tensor(tensor_list, './data/original_data.npz')
+    # unify_save_tensor(replaced_tensor_list, './data/replaced_data.npz')
+
+
 if __name__ == "__main__":
     train_percentage = 0.8
     train_tensor, val_tensor = load_data(train_percentage)
     total_tensor = th.cat([train_tensor, val_tensor], axis=0)
     frequencies = estimate_token_frequency(total_tensor)
     print(frequencies)
+
+    generate_dataset_from_original_levels()
